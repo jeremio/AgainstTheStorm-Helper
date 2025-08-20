41e0f3ef94e7c7803962ca35150ab36ab675695a
Jeremie Cotiniaux <jcotiniaux@gmail.com>
2025-08-20 23:22:01 +0200

    add conf ci

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+// Le nom de votre dépôt GitHub, par exemple "AgainstTheStorm-Helper"
+const GITHUB_REPO_NAME = 'AgainstTheStorm-Helper'
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  // La base doit être le nom de votre dépôt pour que GitHub Pages fonctionne
+  base: process.env.NODE_ENV === 'production' ? `/${GITHUB_REPO_NAME}/` : '/',
   plugins: [vue()],
 })
